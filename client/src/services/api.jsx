dffa70ce4f68ad54546d34a6c42ee714d5723655
Nithin Banothu <bnithinnaik@gmail.com>
2025-11-16 10:56:06 +0530

    api-updated

--- client/src/services/api.jsx
+++ client/src/services/api.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:5000/api";
+const API_BASE =
+  import.meta.env.VITE_API_BASE || "https://lost-found-y6cp.onrender.com";
 
 const client = axios.create({
   baseURL: API_BASE,
